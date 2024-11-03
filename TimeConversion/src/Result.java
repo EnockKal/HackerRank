01fa9469c1298c0a7f7c01a9a47c71afd4e9cea9
Enock <tshims.enock31@gmail.com>
2024-11-03 12:26:41 -0500

    Solution

---
+++ TimeConversion/src/Result.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+        // Write your code here
+        String second = s.substring(6, 8);
+        String min = s.substring(3, 5);
+        int hour = Integer.parseInt(s.substring(0, 2));
+        String period = s.substring(8);
+
+        if(period.equals("AM")){
+            if(hour == 12){
+                hour = 0;
+            }
+        }else if(hour != 12){
+            hour += 12;
+        }
+        return String.format("%02d:%s:%s", hour, min, second);
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
